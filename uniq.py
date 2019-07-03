82ca207e647b9cd1386d414db729cd06640e7ef3
SOBIGARANI <51955251+SOBIGARANI@users.noreply.github.com>
2019-07-03 16:27:38 +0530

    Update uniq.py

--- uniq.py
+++ uniq.py
@@ -1,6 +1,7 @@
 n=int(input())
 a=input().split()
 ar=[]
+s=""
 for i in a:
   ar.append(int(i))
 for i in range(0,n):
@@ -8,5 +9,10 @@ for i in range(0,n):
   for j in range(i+1,n):
     if(ar[i]==ar[j]):
       c=c+1
-  if(c==0):
-    
+  if(c!=0):
+    s=s+str(ar[i])+""
+for i in s:
+  while(int(i) in ar):
+    ar.remove(int(i))
+for i in ar:
+  print(i,end=" ")
