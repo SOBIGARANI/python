7a0e1ce48d9cc5d20bf5aed6165db01fe8bf09a1
SOBIGARANI <51955251+SOBIGARANI@users.noreply.github.com>
2019-07-03 16:42:12 +0530

    Create firstdup.py

---
+++ firstdup.py
@@ -0,0 +1,17 @@
+n=int(input())
+a=input().split()
+ar=[]
+s=""
+for i in a:
+  ar.append(i)
+for i in range(0,n-1):
+  c=0
+  for j in range(i+1,n-1):
+    if(ar[i]==ar[j]):
+      c=c+1
+  if(c!=0 and (ar[i] not in s) and s==""):
+    s=s+str(ar[i])
+if(s==""):
+  print("Unique")
+else:
+  print(s)
