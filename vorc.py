a7bdf5b835aa6e2b06ba3fdb47b21805af41e3f9
SOBIGARANI <51955251+SOBIGARANI@users.noreply.github.com>
2019-07-03 17:02:57 +0530

    Create vorc.py

---
+++ vorc.py
@@ -0,0 +1,9 @@
+a=input()
+v=['a','e','i','o','u','A','E','I','O','U']
+c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+if a in v:
+  print("Vowels")
+elif a in c:
+  print("Consonant")
+else:
+  print("Invalid")
